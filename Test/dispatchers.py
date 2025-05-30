4db4696c48c602ccc13a8790a26d9d29940085b5
dimbo <dimboychuk1995@gmail.com>
2025-05-29 21:49:33 -0500

    added dispatchers list on dispatchers_fragment.html

--- Test/dispatchers.py
+++ Test/dispatchers.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from Test.tools.db import db  # централизованное подключение
 
 dispatchers_bp = Blueprint('dispatchers', __name__)
+users_collection = db['users']
 
 @dispatchers_bp.route('/fragment/dispatchers')
 def dispatchers_fragment():
-    return render_template('fragments/dispatchers_fragment.html')
+    dispatchers = list(users_collection.find({"role": "dispatch"}))
+    return render_template('fragments/dispatchers_fragment.html', dispatchers=dispatchers)
